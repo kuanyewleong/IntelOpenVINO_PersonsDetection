0fb628e4bf8e70c3b8a5855969145883ca4d3a23
Leong Kuan Yew <37063734+kuanyewleong@users.noreply.github.com>
2019-07-24 22:12:04 +0800

    Create person_detection_ssd.hpp

---
+++ code/person_detection_ssd.hpp
@@ -0,0 +1,23 @@
+/*
+This is the header file
+
+*/
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+#include <string>
+#include <vector>
+#include <gflags/gflags.h>
+#include <iostream>
+
+#ifdef _WIN32
+#include <os/windows/w_dirent.h>
+#else
+#include <dirent.h>
+#endif
+
+
+bool FLAGS_auto_resize = false;
+double FLAGS_t = 0.5;
+bool FLAGS_pc = false;
